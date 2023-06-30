27031eeb0b7e7f715033a34bd690f3fa22c71b10
acuenca-facephi <acuenca@facephi.com>
2023-06-30 12:12:40 +0200

    Update README and add k6 example script

--- example-script.js
+++ example-script.js
@@ -1,15 +1,3 @@
-# xk6-read
-[k6](https://github.com/grafana/k6) extension for reading files, implemented using the
-[xk6](https://github.com/grafana/xk6) system.  
-Inspired in the [writing files k6 extension](https://github.com/avitalique/xk6-file).
-
-## Build
-```shell
-xk6 build v0.36.0 --with github.com/acuenca-facephi/xk6-read@latest
-```
-
-## Example
-```javascript
 import read from 'k6/x/read'; 
 
 // change file/directory paths
@@ -25,9 +13,3 @@ export default function () {
     let directoryContent = read.readDirectory(directoryPath);
     console.log(JSON.stringify(directoryContent));
 }
-```
-
-## Run example script
-```shell
-./k6 run example-script.js
-```
